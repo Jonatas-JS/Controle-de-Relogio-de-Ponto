0de675966e5bb79ebe42ce97be600860b5f7389c
Jônatas Fernandes <jonatas.tecnico19@gmail.com>
2024-08-21 13:25:44 -0300

    Created select Predio

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,82 @@
+import { useState } from 'react';
 import { NavBar } from '../../components/NavBar';
 import styles from './Home.module.css'
 
 export function Home() {
+    const prediosSelect = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    const REPs = [
+        {
+            predio_1: [
+                {
+                    predio: '1',
+                    coletor: '',
+                    descricao: 'Predio 1 - I',
+                    rep: 'I',
+                    ip: '',
+                    gr_acesso: '',
+                },
+                {
+                    predio: '1',
+                    coletor: '',
+                    descricao: 'Predio 1 - II',
+                    rep: 'II',
+                    ip: '',
+                    gr_acesso: '',
+                },
+            ],
+            predio_2: [
+                {
+                    predio: '2',
+                    coletor: '',
+                    descricao: 'Predio 2 - I',
+                    rep: 'I',
+                    ip: '',
+                    gr_acesso: '',
+                },
+                {
+                    predio: '2',
+                    coletor: '',
+                    descricao: 'Predio 2 - II',
+                    rep: 'II',
+                    ip: '',
+                    gr_acesso: '',
+                },
+            ],
+            predio_3: [
+                {
+                    predio: '3',
+                    coletor: '',
+                    descricao: 'Predio 3 - I',
+                    rep: 'I',
+                    ip: '',
+                    gr_acesso: '',
+                },
+                {
+                    predio: '3',
+                    coletor: '',
+                    descricao: 'Predio 3 - II',
+                    rep: 'II',
+                    ip: '',
+                    gr_acesso: '',
+                },
+            ],
+        },
+
+    ];
+
+    console.log(REPs[0].predio_1[0].descricao)
+
+    const [selectedOption, setSelectedOption] = useState('selectedRegistro');
+
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target);
+    };
+
+    function handlePredioSelected(event) {
+        console.log(event.target)
+    };
+
     return (
         <div className={styles.container}>
             <NavBar />
@@ -10,15 +85,44 @@ export function Home() {
                     <div className={styles.primaryServiceSelection}>
                         <div className={styles.checkboxRegistro}>
                             <label>
-                                <input type='checkbox' checked />Registro
+                                <input
+                                    type='radio'
+                                    value='selectedRegistro'
+                                    checked={selectedOption === 'selectedRegistro'}
+                                    onChange={handleOptionChange}
+                                />Registro
                             </label>
                         </div>
                         <div className={styles.checkboxRemocao}>
                             <label>
-                                <input type='checkbox'/>Remoção
+                                <input
+                                    type='radio'
+                                    value='selectedRemocao'
+                                    checked={selectedOption === 'selectedRemocao'}
+                                    onChange={handleOptionChange}
+                                />Remoção
                             </label>
                         </div>
 
+                    </div>
+                    <div className={styles.selectionREPs}>
+                        <div>
+                            <div className={styles.predioSelect}>
+                                {prediosSelect.map((item) => {
+                                    return (
+                                        <div
+                                            className={styles.predioList}
+                                            key={Math.random()}
+                                            value={item}
+                                            onClick={handlePredioSelected}
+                                        >
+                                            <p>{item}</p>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
